a030b1a1d59a9d847d4483e9e2c4d05525c60bee
mutahharnazir <memehaynoon@gmail.com>
2026-08-30 13:09:13 +1000

    Update rewrite_the_ocean_slversion.py

--- rewrite_the_ocean_slversion.py
+++ rewrite_the_ocean_slversion.py
@@ -56,6 +56,7 @@ with st.sidebar:
     
     st.markdown("- The total number of letters will be 9.")
     st.markdown("- You may choose between 3 and 5 vowels (the rest will be consonants).")
+    st.image("bondibeach.jpg")
     st.markdown("- Your aim is to make the longest possible word.")
     st.markdown("- If the word you enter is related to the ocean, you get bonus points.")
     st.markdown("- The score is tripled if you make a word using all nine letters.")
