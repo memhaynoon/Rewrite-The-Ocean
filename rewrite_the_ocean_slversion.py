ddc2835d0ecce3b876b63088ee84b9735c8679a1
mutahharnazir <memehaynoon@gmail.com>
2026-09-06 22:32:13 +1000

    UpdateConsonantsToFullList

--- rewrite_the_ocean_slversion.py
+++ rewrite_the_ocean_slversion.py
@@ -93,7 +93,7 @@ for key, value in defaults.items():
 def generate_letters(vowel_amount):
     consonant_amount = 9 - vowel_amount
     vowels = ["e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "a", "a", "a","a", "a", "a", "a", "a", "a", "i", "i", "i", "i", "i", "i", "i", "i", "i", "o", "o", "o", "o", "o", "o", "o", "o", "u", "u", "u", "u"]
-    consonants = [ "w", "y", "y", "k", "j", "x", "q", "z"
+    consonants = ["n", "n", "n", "n", "n", "n", "r", "r", "r", "r", "r", "r", "t", "t", "t", "t", "t", "t", "d", "d", "d", "d", "l", "l", "l", "l", "s", "s", "s", "s", "g", "g", "g", "b", "b", "c", "c", "f", "f", "h", "h", "m", "m", "p", "p", "v", "v", "w", "w", "y", "y", "k", "j", "x", "q", "z"
 ]
 
     vowels_decided = [random.choice(vowels) for _ in range(5)]
